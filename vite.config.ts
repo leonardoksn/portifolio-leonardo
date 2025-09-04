ae12c61892a88fc0dc07db920ee9913de1e6fe33
leonardoksn <leonardo.ksn@gmail.com>
2025-09-03 21:24:02 -0300

    feat: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [tailwindcss(), tanstackRouter({}), react()],
+	base: "/portifolio-leonardo",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
